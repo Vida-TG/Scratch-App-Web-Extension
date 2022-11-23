fc3443f47fa850ff2c565766e4e9453a9d96b3f9
ViDA_TG <victorademiju.t@gmail.com>
2022-11-23 03:32:32 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let listOfTxt = [];
+let inputBox = document.querySelector('#input-el');
+let inputSubmit = document.querySelector('#input-btn');
+let refreshSubmit = document.querySelector('#refresh-btn');
+let inputList = document.querySelector('#input-list')
+
+inputSubmit.addEventListener("click", function(e){
+    if (inputBox.value){
+        if(listOfTxt.length == 10){listOfTxt.shift()}
+        listOfTxt.push(inputBox.value);
+        localStorage.setItem("saveExList", JSON.stringify(listOfTxt));
+        listOfTxt = JSON.parse(localStorage.getItem("saveExList"));
+        inputList.innerHTML = "";
+        addText(listOfTxt);
+        inputBox.value = "";
+        
+    }
+
+})
+
+refreshSubmit.addEventListener("click", function(e){
+    listOfTxt = JSON.parse(localStorage.getItem("saveExList"));
+    inputList.innerHTML = "";
+    addText(listOfTxt);
+})
+
+function addText(listOfTxt){
+    for(let i=0; i<(listOfTxt.length); i++){
+        if((listOfTxt[i].startsWith("https://"))){
+            inputList.innerHTML += "<li><a href='"+listOfTxt[i]+"' target='_blank'>"+listOfTxt[i]+"</a></li>"
+        }else if((listOfTxt[i].startsWith("www."))){
+            inputList.innerHTML += "<li><a href='https://"+listOfTxt[i]+"' target='_blank'>"+listOfTxt[i]+"</a></li>"
+        }else{
+            inputList.innerHTML += "<li>"+listOfTxt[i]+"</li>"
+        }
+    }
+}
